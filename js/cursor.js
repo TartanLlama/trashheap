b3e410898e1529ca0f3004641c1377abf420f692
TartanLlama <TartanLlama@users.noreply.github.com>
2019-11-19 14:10:50 +0000

    Automated deployment: Tue Nov 19 14:10:50 UTC 2019 208fbbc7e1a858da282c77225cb6eda695a11da1

--- js/cursor.js
+++ js/cursor.js
@@ -57,68 +57,6 @@ a=(ns||n6)?'':'all.';
 n6r=(n6)?'")':'';
 s=(ns)?'':'.style';
 
-if (ns){
-	for (i = 0; i < n; i++)
-		document.write('<layer name="dots'+i+'" top=0 left=0 width='+i/2+' height='+i/2+' bgcolor=#ff0000></layer>');
-}
-
-toWrite = '';
-
-if (ie)
-	toWrite += '<div id="con" style="position:absolute;top:0px;left:0px"><div style="position:relative">';
-
-if (ie||n6){
-	for (i = 0; i < n; i++)
-		toWrite+='<div id="dots'+i+'" style="position:absolute;pointer-events:none;top:0px;left:0px;width:'+i/2+'px;height:'+i/2+'px;background:#ff0000;font-size:'+i/2+'"></div>';
-}
-
-if (ie)
-	toWrite+='</div>';
-
-document.write(toWrite);
-(ns||n6)?window.captureEvents(Event.MOUSEMOVE):0;
-
-function Mouse(evnt){
-
-	y = (ns||n6)?evnt.pageY+4 - window.pageYOffset:event.y+4;
-	x = (ns||n6)?evnt.pageX+1:event.x+1;
-}
-
-(ns)?window.onMouseMove=Mouse:document.onmousemove=Mouse;
-
-function animate(){
-
-	o=(ns||n6)?window.pageYOffset:0;
-
-	if (ie)con.style.top=document.body.scrollTop + 'px';
-
-	for (i = 0; i < n; i++){
-
-		var temp1 = eval(d+a+"dots"+i+n6r+s);
-
-		randcolours = colours[Math.floor(Math.random()*colours.length)];
-
-		(ns)?temp1.bgColor = randcolours:temp1.background = randcolours; 
-
-		if (i < n-1){
-
-			var temp2 = eval(d+a+"dots"+(i+1)+n6r+s);
-			temp1.top = parseInt(temp2.top) + 'px';
-			temp1.left = parseInt(temp2.left) + 'px';
-
-		} 
-		else{
-
-			temp1.top = y+o + 'px';
-			temp1.left = x + 'px';
-		}
-	}
-
-	setTimeout("animate()",10);
-}
-
-animate();
-
 window.onload=function() { if (document.getElementById) {
 	var i, rats, rlef, rdow;
 	for (var i=0; i<sparkles; i++) {
