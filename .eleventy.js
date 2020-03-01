86d93fd70e65f6f70b869cb70208378782e04b81
TartanLlama <TartanLlama@users.noreply.github.com>
2020-03-01 11:04:26 +0000

    Automated deployment: Sun Mar  1 11:04:26 UTC 2020 9714f8c3bda4b3ee18ca6c16d478cdb10a211e64

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,12 @@
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addCollection("issue0sorted", (collection) =>
+  collection.getFilteredByGlob("issues/TR0SH/*.md").sort((a, b) => {
+    if (a.data.author > b.data.author) return 1;
+    else if (a.data.author < b.data.author) return -1;
+    else return 0;
+  })
+);
+
     return {
       templateFormats: [
         "jpg", "md", "css", "cur", "html", "png", "gif", "js"
